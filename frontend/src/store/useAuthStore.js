60bc1c9a7acf900e31654c03aa5e6ea762f84116
Varun <127723672+VARUNx30@users.noreply.github.com>
2026-08-20 01:24:46 +0530

    Refactored App component to implement routing  & Created axios instance for API calls

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoginIng: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({authUser: res.data}); 
+        } catch {
+            set({authUser: null});
+        } finally{
+            set({isCheckingAuth: false});
+        }
+    }
+}))
