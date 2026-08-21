c6a167d9618a51a098d7a123d23021d08c9ee473
Varun <127723672+VARUNx30@users.noreply.github.com>
2026-08-22 03:05:08 +0530

    Implemented Navbar and Login pages with UI and authentication

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -13,32 +13,45 @@ export const useAuthStore = create((set) => ({
         try {
             const res = await axiosInstance.get("/auth/check");
 
-            set({authUser: res.data}); 
+            set({ authUser: res.data });
         } catch {
-            set({authUser: null});
-        } finally{
-            set({isCheckingAuth: false});
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
         }
     },
 
     signup: async (data) => {
-        set ({ isSigningUp: true});
+        set({ isSigningUp: true });
         try {
             const res = await axiosInstance.post("/auth/signup", data);
-            set({authUser: res.data});
+            set({ authUser: res.data });
             toast.success("Account created successfully");
 
         } catch (error) {
             toast.error(error.response?.data?.message || "Unable to create account");
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoginIng: true });
+        try {
+            const res = await axiosInstance.post("auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
         } finally{
-            set({ isSigningUp: false});
+            set({ isLoginIng: false });
         }
     },
 
     logout: async () => {
         try {
             await axiosInstance.post("/auth/logout");
-            set({authUser: null});
+            set({ authUser: null });
             toast.success("Logged out successfully");
         } catch (error) {
             toast.error(error.response.data.message);
