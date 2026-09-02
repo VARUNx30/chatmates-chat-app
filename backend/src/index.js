4ede63d2a3e4d572fdf4be9f661af7bbc996a4c5
Varun <127723672+VARUNx30@users.noreply.github.com>
2026-09-02 18:36:12 +0530

    Implemented socket.io for real-time online user tracking

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,12 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser'
 import messageRoutes from './routes/message.route.js'
 import cors from 'cors'
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
@@ -20,16 +20,10 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const startServer = async () => {
-    try {
-        await connectdb();
 
-        app.listen(PORT, () => {
-            console.log("Server is running on PORT: " + PORT);
-        });
-    } catch (error) {
-        console.error("Server failed to start:", error.message);
-    }
-};
 
-startServer();
+server.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectdb();
+});
+
