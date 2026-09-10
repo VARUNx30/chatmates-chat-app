d621a33a4f6838e77e566a42451186b8d9606a6f
Varun <127723672+VARUNx30@users.noreply.github.com>
2026-09-10 17:08:22 +0530

    Fix typo in build script name

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -59,7 +59,7 @@ const MessageInput = () => {
             <button
               onClick={removeImage}
               className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
-              flex items-center justify-center"
+              flex items-center justify-center cursor-pointer"
               type="button"
             >
               <X className="size-3" />
